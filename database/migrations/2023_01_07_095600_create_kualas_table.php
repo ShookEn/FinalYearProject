cc526e376d7ec0021298d2e3636b33c1197064a0
ShookEn <80733443+ShookEn@users.noreply.github.com>
2023-02-09 13:53:14 +0800

    Add files via upload

---
+++ database/migrations/2023_01_07_095600_create_kualas_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKualasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kualas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->double('price',8,2);
+            $table->string('image');
+            $table->string('image_a');
+            $table->string('image_b');
+            $table->string('image_c');
+            $table->integer('quantity')->unsigned();
+            $table->string('categoryID');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kualas');
+    }
+}
